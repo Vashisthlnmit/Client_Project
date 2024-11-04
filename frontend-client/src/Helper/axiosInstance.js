ca0e04aa14dff9fd87a8708d7c3d05a6c80450d0
Vashisthlnmit <vashisthkumar54@gmail.com>
2024-11-04 20:44:37 +0530

    second update

--- frontend-client/src/Helper/axiosInstance.js
+++ frontend-client/src/Helper/axiosInstance.js
@@ -3,4 +3,5 @@ const Base_URL="http://localhost:4000";
 const axiosInstance=axios.create();
 axiosInstance.defaults.baseURL=Base_URL;
 axiosInstance.defaults.withCredentials=true;
+//axiosInstance.interceptors.request.
 export default axiosInstance;
--- frontend-client/src/main.jsx
+++ frontend-client/src/main.jsx
@@ -5,12 +5,28 @@ import store from './Store/store.js'
 import {Provider} from "react-redux"
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './layout.jsx'
+import Signin from './Components/Signin.jsx'
+import Contactus from './Components/Contactus.jsx'
+import ResetPassword from './Components/ResetPassword.jsx'
+import FirstPage from './Components/SigninFirstPage.jsx'
 const router=createBrowserRouter(
   createRoutesFromElements(
+    <>
+    <Route element={<Layout/>}>
+      <Route path="/signin" element={<Signin/>}/>
+      <Route path="/contact" element={<Contactus/>}/>
+      <Route path='/reset' element={<ResetPassword/>}/>
+    </Route>
+    <Route path='/' element={<FirstPage/>}>
+
+    </Route>
+    </>
   )
 )
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router}>
+      </RouterProvider>
   </Provider>,
 )
